84094d1d38419675d6690ccaf9c31fb17952a3f4
Andrew Becker <abecks@gmail.com>
2022-10-20 09:53:07 -0700

    first commit

---
+++ src/matchers/toDisplayDialog.test.js
@@ -0,0 +1,13 @@
+describe('toDisplayDialog', () => {
+  beforeEach(async () => {
+    await page.goto(`http://localhost:${process.env.TEST_SERVER_PORT}`)
+  })
+
+  it('should handle dialog', async () => {
+    const dialog = await expect(page).toDisplayDialog(async () => {
+      await page.click('#dialog-btn')
+    })
+    expect(dialog.message()).toBe('Bouh!')
+    await dialog.accept()
+  })
+})
